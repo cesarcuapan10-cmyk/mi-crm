76f493cccc0837679f02c1e172218af4c9fe13f2
Cesar Cuapantecatl Sanchez <cesarcuapantecatlsanchez@MacBook-Air-de-Cesar.local>
2026-06-26 20:54:06 -0600

    Restaurar middleware de autenticacion

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,22 @@
+import NextAuth from 'next-auth'
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { authConfig } from '@/auth.config'
 
-export function middleware(req: NextRequest) {
-  return NextResponse.next()
-}
+const { auth } = NextAuth(authConfig)
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth
+  const path = req.nextUrl.pathname
+  const isAuthPage = path.startsWith('/login')
+
+  if (!isLoggedIn && !isAuthPage) {
+    return NextResponse.redirect(new URL('/login', req.nextUrl))
+  }
+
+  if (isLoggedIn && isAuthPage) {
+    return NextResponse.redirect(new URL('/', req.nextUrl))
+  }
+})
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|landing|agenda).*)'],
